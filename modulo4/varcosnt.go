47192392b1a71039a021409072d4f2748db46606
Marlon <marloncostaesousa@gmail.com>
2024-08-06 08:48:22 -0300

    Adicionando conteúdo sobre variáveis e constantes

---
+++ modulo4/varcosnt.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Variáveis
+	var nome string
+	nome = "Billy"
+	fmt.Println(nome)
+
+	nome = "Marlon"
+	fmt.Println(nome)
+
+	var idade int
+	idade = 33
+	fmt.Println(idade)
+
+	var a = "Costa"
+	fmt.Println(a)
+
+	var b, c = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	f := "Apple"
+	fmt.Println(f)
+
+	numero := 12
+	fmt.Println(numero)
+
+	const idadeBilly = 1.6
+	fmt.Println(idadeBilly)
+}
