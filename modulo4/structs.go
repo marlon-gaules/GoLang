3f663e81e797387a2713949f86b4b7b9628f21d0
Marlon <marloncostaesousa@gmail.com>
2024-08-12 08:41:56 -0300

    Adicionando conteúdo sobre structs part2

--- modulo4/structs.go
+++ modulo4/structs.go
@@ -12,4 +12,18 @@ type Pessoa struct {
 func main() {
 	fmt.Println(Pessoa{"Marlon", "Sousa", 33, 1991})
 	fmt.Println(Pessoa{Nome: "Marlon", Sobrenome: "Sousa", Idade: 33, AnoNasc: 1991})
+	fmt.Println(Pessoa{Nome: "Marlon"})
+
+	p1 := Pessoa{Sobrenome: "Araujo", AnoNasc: 2023}
+	fmt.Println(p1.Sobrenome)
+	fmt.Println(p1.AnoNasc)
+
+	p1.Sobrenome = "Sousa"
+	fmt.Println(p1.Sobrenome)
+
+	p2 := Pessoa{Sobrenome: "Costa", AnoNasc: 2020}
+
+	pessoas := []Pessoa{}
+	pessoas = append(pessoas, p1, p2)
+	fmt.Println(pessoas)
 }
