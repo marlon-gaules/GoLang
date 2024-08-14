1b8d0a7a5f5b0bf75c8c6b9d17207e592d6167c4
Marlon <marloncostaesousa@gmail.com>
2024-08-14 20:30:22 -0300

    Adicionando conteúdo sobre if-else part2

--- modulo4/if-else.go
+++ modulo4/if-else.go
@@ -4,8 +4,32 @@ import "fmt"
 
 func main() {
 
-	valor := 1
+	valor := 4
 	if valor == 1 {
 		fmt.Println("Valor é igual a 1")
+	} else {
+		fmt.Println("Valor não é igual a 1")
+	}
+
+	numero := 9
+	if numero == 1 {
+		fmt.Println("Numero é igual a 1")
+	} else if numero == 5 {
+		fmt.Println("Numero é igual a 5")
+	} else {
+		fmt.Println("Valor é diferente de 1 e 5")
+	}
+
+	fmt.Println(1 + 1)
+	fmt.Println(3 - 1)
+	fmt.Println(3 * 4)
+	fmt.Println(1 / 1)
+	fmt.Println(10 % 2)
+
+	number := 8
+	if number%2 == 0 {
+		fmt.Printf("%d é par", number)
+	} else {
+		fmt.Printf("%d é impar", number)
 	}
 }
