1e289458fceff97b1a17c449e1acab979c330f45
Marlon <marloncostaesousa@gmail.com>
2024-08-16 20:11:02 -0300

    Adicionando exercício do codewars

--- modulo5/exercicio1.go
+++ modulo5/exercicio1.go
@@ -2,6 +2,8 @@ package kata
 
 func MakeNegative(x int) int {
 
+	//Nesta tarefa simples, você recebe um número e tem que torná-lo negativo. Mas talvez o número já seja negativo?
+
 	if x <= 0 {
 		return x
 	}
