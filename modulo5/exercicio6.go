a241eaa2295d5e543df0a3e991c1cdc34cea6eed
Marlon <marloncostaesousa@gmail.com>
2024-08-21 07:31:00 -0300

    Adicionando comentário no exercício 6 do codewars

--- modulo5/exercicio6.go
+++ modulo5/exercicio6.go
@@ -3,6 +3,9 @@ package kata
 import "strconv"
 
 func NumberToString(n int) string {
+
+	// Precisamos de uma função que possa transformar um número (inteiro) em uma string.
+
 	numberString := strconv.Itoa(n)
 	return numberString
 }
